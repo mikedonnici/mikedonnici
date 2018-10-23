6660284ac2bedbb7d292f131d165f3f186116123
Mike Donnici <michael@mesa.net.au>
2018-10-23 14:44:39 +1100

    update title, description

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: "Mike Donnici",
+    title: "Mike Donnici - Jervis Bay, NSW",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
