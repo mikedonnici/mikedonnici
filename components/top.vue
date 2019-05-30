14f63572b5f329992f16ccf098ba6ca2497413a6
Mike Donnici <michael@mesa.net.au>
2019-05-31 08:18:03 +1000

    projects page

---
+++ components/top.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="hero is-small is-warning is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-mobile is-vcentered">
+            <div class="column is-narrow">
+              <figure class="image is-96x96">
+                <img src="~/assets/mike.png">
+              </figure>
+            </div>
+            <div class="column">
+              <h2 class="title is-2 is-size-4-mobile">Mike Donnici</h2>
+              <h4 class="subtitle is-4 is-size-6-mobile">Full-stack web developer</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,7 @@
 <template>
   <div>
-    <section class="hero is-small is-warning is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <div class="columns is-mobile is-vcentered">
-            <div class="column is-narrow">
-              <figure class="image is-96x96">
-                <img src="~/assets/mike.png">
-              </figure>
-            </div>
-            <div class="column">
-              <h2 class="title is-2 is-size-4-mobile">Mike Donnici</h2>
-              <h4 class="subtitle is-4 is-size-6-mobile">Full-stack web developer</h4>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+
+    <Top/>
 
     <section class="section">
       <div class="container">
@@ -308,6 +293,9 @@
 </template>
 
 <script>
+
+import Top from "~/components/top"
+
 export default {
   head() {
     return {
@@ -322,6 +310,9 @@ export default {
         }
       ]
     };
+  },
+  components: {
+    Top,
   },
   data() {
     return {
